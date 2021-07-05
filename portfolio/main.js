62c579277b0f4bdc0b0971c1869591eca9cc30b5
EomNamyoung <32608891+EomNamyoung@users.noreply.github.com>
2021-07-05 17:34:28 +0900

    JS: Navbar menu toggle& Project UI

--- portfolio/main.js
+++ portfolio/main.js
@@ -10,6 +10,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=> {
@@ -18,9 +19,15 @@ navbarMenu.addEventListener('click', (event)=> {
     if(link == null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
+//navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
+});
 
 //Handle click on "contact me" button on home
 const contactbtn = document.querySelector('.home_contact');
@@ -86,6 +93,7 @@ workBtnContainer.addEventListener('click', (e)=> {
     }, 300);
 
 });
+
 
 
 function scrollIntoView(selector){
