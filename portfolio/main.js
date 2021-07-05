5b37116b5e36cfd05bce32d951b970d15565488f
EomNamyoung <32608891+EomNamyoung@users.noreply.github.com>
2021-07-05 13:17:44 +0900

    JS: Arrow-up

--- portfolio/main.js
+++ portfolio/main.js
@@ -36,10 +36,27 @@ document.addEventListener('scroll', ()=>{
 });
 
 
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2){
+        arrowUp.classList.add('visible')
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//Handle click on the "arrow up"button
+arrowUp.addEventListener('click', () =>{
+    scrollIntoView('#home');
+}); 
+
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:"smooth"});
 }
-//홈화면 불투명하게 만들기
+
 
 
